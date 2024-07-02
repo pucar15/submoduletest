5cf301cc39dcef652011aaf6134d6ac25b55e55d
pucar15 <167426436+pucar15@users.noreply.github.com>
2024-07-02 15:25:30 +0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Odoo version test',
+    'version': '15.0.1.0.0',
+    'summary': 'odoo version test',
+    'author': 'test',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
